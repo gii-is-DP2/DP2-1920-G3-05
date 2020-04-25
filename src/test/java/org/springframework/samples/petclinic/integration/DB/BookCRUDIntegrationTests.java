d2f9c637365edde954fcf83211926c512b00362e
Juan <juaferfer11@alum.us.es>
2020-04-25 14:28:32 +0200

    Juan 25/04/2020 fixed test configuration

--- src/test/java/org/springframework/samples/petclinic/integration/DB/BookCRUDIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/integration/DB/BookCRUDIntegrationTests.java
@@ -2,30 +2,26 @@ package org.springframework.samples.petclinic.integration.DB;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.model.Book;
 import org.springframework.samples.petclinic.model.Genre;
 import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.repository.BookRepository;
 import org.springframework.samples.petclinic.repository.UserRepository;
 import org.springframework.samples.petclinic.util.EntityUtils;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ActiveProfiles;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(
-webEnvironment=SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-mysql.properties")
+@ActiveProfiles("mysql")
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 public class BookCRUDIntegrationTests {
 	@Autowired
     private BookRepository sut;
@@ -34,7 +30,7 @@ public class BookCRUDIntegrationTests {
 
     @ParameterizedTest
 	@CsvSource({
-		"prueba,antonio,SM,9788425223280,574,Esto es una prueba,admin1,15","prueba2,pepe,Casals,9781234567897,123,Esto es otra prueba,admin1,16"
+		"prueba,antonio,SM,9788425223280,574,Esto es una prueba,admin1,15","prueba2,pepe,Casals,9781234567897,123,Esto es otra prueba,admin1,15"
 	})
     public void shouldCreateBook(String title,String author,String editorial,String isbn,Integer pages,String synopsis,String username,int size) {
     	User user = this.userRepo.findByUsername(username);
@@ -68,8 +64,8 @@ public class BookCRUDIntegrationTests {
     
     @ParameterizedTest
 	@CsvSource({
-		"14, 15",
-		"13, 14",
+		"14, 13",
+		"13, 13",
 	})
 	void shouldDeleteBook(Integer bookId,Integer size){
 		this.sut.deleteBookById(bookId);

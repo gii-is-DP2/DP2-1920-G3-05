fec99f0ddd5b4a3cccf6700144f271785fda11f1
Javier <javi77.jgc@gmail.com>
2020-05-23 19:46:29 +0200

    Refactor: API cache
    Used cache to avoid use API too much

--- src/main/java/org/springframework/samples/petclinic/service/PoemService.java
+++ src/main/java/org/springframework/samples/petclinic/service/PoemService.java
@@ -1,5 +1,13 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.samples.petclinic.model.Poem;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -7,11 +15,27 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class PoemService {
 
-    public Poem getRandomPoem() {
+	@Autowired
+	private PoemService poemService;
+	
+    @Cacheable("poemList")
+    public List<Poem> getPoemsList() {
+        List<Poem> poems = new ArrayList<>();
         final String uri = "https://www.poemist.com/api/v1/randompoems";
-		RestTemplate restTemplate = new RestTemplate();
-        Poem[] poem = restTemplate.getForObject(uri, Poem[].class);
-        return poem[0];
+        RestTemplate restTemplate = new RestTemplate();
+        while(poems.size()<10){
+            Poem[] poem = restTemplate.getForObject(uri, Poem[].class);
+            List<Poem> aux = Arrays.asList(poem);
+            poems.addAll(aux);
+        }
+        return poems;
+    }
+
+    public Poem getRandomPoem() {
+        List<Poem> poems = poemService.getPoemsList();
+		int random = ThreadLocalRandom.current().nextInt(0,10);
+		Poem poem = poems.get(random);
+		return poem;
     }
     
 } 
